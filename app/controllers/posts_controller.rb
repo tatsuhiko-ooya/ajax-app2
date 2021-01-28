bbb917392bd9846f07b6f1f4759214fc02563918
tatsuhiko_ooya <tatsuhiko.ooya.prog@gmail.com>
2021-01-29 03:32:30 +0900

    既読機能のフロント機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,7 +17,7 @@ class PostsController < ApplicationController
     else
       post.update(checked: true)
     end
-    item = Post.find(param[:id])
+    item = Post.find(params[:id])
     render json: {post: item}
   end
 
