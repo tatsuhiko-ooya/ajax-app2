a8a7176e6b2b9ccc0e2101d1f5cb157e80d20ae0
tatsuhiko_ooya <tatsuhiko.ooya.prog@gmail.com>
2021-01-29 00:03:05 +0900

    Ajaxappの事前準備

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_12_23_192655) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.boolean "checked"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
